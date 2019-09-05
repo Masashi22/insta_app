a63487a886384ecec93ff87b801b93268bd64038
Masashi <t15519ms@gmail.com>
2019-09-05 21:48:30 +0900

    Add title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #タイトルを返す
+  def full_title(page_title = '')
+    base_title = 'Instagram'
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
