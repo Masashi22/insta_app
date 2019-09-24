6db74c9f9fcec929ab90955b8cc940413a6b630b
Masashi <t15519ms@gmail.com>
2019-09-24 21:13:08 +0900

    Modified micropost_spec

--- spec/features/micropost_spec.rb
+++ spec/features/micropost_spec.rb
@@ -5,11 +5,19 @@ RSpec.feature "micropost", :devise do
     @user = create(:user)
   end
 
-  scenario "successfully" do
+  scenario "is successfully" do
     login(@user)
     expect(current_path).to eq user_path(@user)
     fill_in "textarea1", with: "sample text"
     expect { click_on "Post" }.to change { Micropost.count }.by(1)
     expect(page).to have_content('sample text')
+  end
+
+  scenario "is fail because content is blank" do
+    login(@user)
+    expect(current_path).to eq user_path(@user)
+    fill_in "textarea1", with: " "
+    click_on "Post"
+    expect(current_path).to eq microposts_path
   end
 end
