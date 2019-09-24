8566b4c79abf9e0465b7c10d6bbd39ed81fe09a1
Masashi <t15519ms@gmail.com>
2019-09-24 23:59:07 +0900

    Modified micropost descending

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @microposts = @user.microposts.page(params[:page])
+    @microposts = @user.microposts.recent.page(params[:page])
     @micropost = current_user.microposts.build
   end
 end
