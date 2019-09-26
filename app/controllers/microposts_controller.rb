4cdc9cfc19cd12d73b0663bc2eb8e866f7a01f5f
Masashi <t15519ms@gmail.com>
2019-09-26 21:37:55 +0900

    Add picture uploader

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -26,6 +26,6 @@ class MicropostsController < ApplicationController
   private
 
   def micropost_params
-    params.require(:micropost).permit(:content)
+    params.require(:micropost).permit(:content, :picture)
   end
 end
