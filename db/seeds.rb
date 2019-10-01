bbc89d2bda8c4604b6adef81db058811a6d8e181
Masashi <t15519ms@gmail.com>
2019-10-01 19:00:38 +0900

    Add relationship model

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,8 +15,9 @@ User.create!(username: "Example User",
   user.save
 end
 
-users = User.order(:created_at).take(6)
-50.times do
-  content = Faker::Lorem.sentence(5)
-  users.each { |user| user.microposts.create!(content: content) }
-end
+users = User.all
+user  = users.first
+following = users[2..10]
+followers = users[4..15]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
