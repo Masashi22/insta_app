c2e78403c9d136063c77106833c1503067fed802
Masashi <t15519ms@gmail.com>
2019-10-02 15:54:33 +0900

    Add following & followers page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:index, :show]
+  before_action :authenticate_user!, only: [:index, :show, :following, :followers]
   PER = 5
   def index
     @users = User.page(params[:page]).per(PER)
@@ -9,5 +9,19 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @microposts = @user.microposts.recent.page(params[:page])
     @micropost = current_user.microposts.build
+  end
+
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.following.page(params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.page(params[:page])
+    render 'show_follow'
   end
 end
