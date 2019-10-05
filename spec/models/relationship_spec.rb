292d13f97b090598b7c49a78b4c8c1f38ef12faf
Masashi <t15519ms@gmail.com>
2019-10-05 21:46:43 +0900

    Add rspec to relationship

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "follow & unfollow" do
+    let(:user) { create(:user) }
+    let(:other_user) { create(:user) }
+
+    it "follow" do
+      user.follow(other_user)
+      expect(user.following.count).to eq 1
+    end
+
+    it "unfollow" do
+      user.follow(other_user)
+      expect(user.following.count).to eq 1
+      user.unfollow(other_user)
+      expect(user.following.count).to eq 0
+    end
+
+    it "following?" do
+      user.follow(other_user)
+      expect(user.following.count).to eq 1
+      expect(user.following?(other_user)).to eq true
+    end
+  end
 end
